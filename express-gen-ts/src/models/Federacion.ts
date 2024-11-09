a9b6f6a7679e046a23f77359085d638f133b7a1c
JoaquinBardelli <alumno24.bardelli.joaquin@ipm.edu.ar>
2024-11-09 12:40:08 -0300

    Inicio guardado de equipos en BD

---
+++ express-gen-ts/src/models/Federacion.ts
@@ -0,0 +1,67 @@
+import moment from 'moment';
+
+// **** Variables **** //
+
+const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an object ' + 
+  'with the appropriate user keys.';
+
+
+// **** Types **** //
+
+export interface IFederacion {
+    id: number;
+    nombre: string;
+    clubes: string[];
+}
+
+
+// **** Functions **** //
+
+/**
+ * Create new User.
+ */
+function new_(
+    nombre?: string,
+    clubes?: string[],
+    id?: number, // id last cause usually set by db
+    ): IFederacion {
+    return {
+        id: (id ?? -1),
+        nombre: (nombre ?? ''),
+        clubes: (clubes ?? []),
+    };
+}
+
+/**
+ * Get user instance from object.
+ */
+function from(param: object): IFederacion {
+    if (!isFederacion(param)) {
+        throw new Error(INVALID_CONSTRUCTOR_PARAM);
+    }
+    const p = param as IFederacion;
+    return new_(p.nombre, p.clubes, p.id);
+}
+
+/**
+ * See if the param meets criteria to be a user.
+ */
+function isFederacion(arg: unknown): boolean {
+    return (
+        !!arg &&
+        typeof arg === 'object' &&
+        'id' in arg && typeof (arg as any).id === 'number' &&
+        'nombre' in arg && typeof (arg as any).nombre === 'string' &&
+        'clubes' in arg && Array.isArray((arg as any).clubes)
+    );
+}
+
+
+
+// **** Export default **** //
+
+export default {
+  new: new_,
+  from,
+  isFederacion,
+} as const;
--- express-gen-ts/src/repos/Mongoose.ts
+++ express-gen-ts/src/repos/Mongoose.ts
@@ -4,189 +4,197 @@ import { IUsuario } from "@src/models/Usuario";
 import Mongoose, { Connection, Model, Schema } from "mongoose";
 import { Console } from "console";
 import exp from "constants";
-import  { IJugador } from "@src/models/Jugador";
+import { IJugador } from "@src/models/Jugador";
+import { IFederacion } from "@src/models/Federacion";
 
 // **** Variables **** //
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //var uniqueValidator = require('mongoose-unique-validator');
 // **** Types **** //
 const tirosSchema = new Mongoose.Schema({
-    tirosDeCampo: {
-      type: Number,
-      required: false
-    },
-    tirosDeCampoConvertidos: {
-      type: Number,
-      required: false
-    },
-    tirosDeDos: {
-      type: Number,
-      required: false
-    },
-    tirosDeDosConvertidos: {
-      type: Number,
-      required: false
-    },
-    tirosDeTres: {
-      type: Number,
-      required: false
-    },
-    tirosDeTresConvertidos: {
-      type: Number,
-      required: false
-    },
-    tirosLibres: {
-      type: Number,
-      required: false
-    },
-    tirosLibresConvertidos: {
-      type: Number,
-      required: false
-    }
+  tirosDeCampo: {
+    type: Number,
+    required: false,
+  },
+  tirosDeCampoConvertidos: {
+    type: Number,
+    required: false,
+  },
+  tirosDeDos: {
+    type: Number,
+    required: false,
+  },
+  tirosDeDosConvertidos: {
+    type: Number,
+    required: false,
+  },
+  tirosDeTres: {
+    type: Number,
+    required: false,
+  },
+  tirosDeTresConvertidos: {
+    type: Number,
+    required: false,
+  },
+  tirosLibres: {
+    type: Number,
+    required: false,
+  },
+  tirosLibresConvertidos: {
+    type: Number,
+    required: false,
+  },
 });
 
 const estadisticasSchema = new Mongoose.Schema({
   minutosJugados: {
     type: Number,
-    required: false
+    required: false,
   },
   segundosJugados: {
     type: Number,
-    required: false
+    required: false,
   },
   puntos: {
     type: Number,
-    required: false
+    required: false,
   },
   rebotesOfensivos: {
     type: Number,
-    required: false
+    required: false,
   },
   rebotesDefensivos: {
     type: Number,
-    required: false
+    required: false,
   },
   asistencias: {
     type: Number,
-    required: false
+    required: false,
   },
   faltasCometidas: {
     type: Number,
-    required: false
+    required: false,
   },
   faltasRecibidas: {
     type: Number,
-    required: false
+    required: false,
   },
   taponesCometidos: {
     type: Number,
-    required: false
+    required: false,
   },
   taponesRecibidos: {
     type: Number,
-    required: false
+    required: false,
   },
   perdidas: {
     type: Number,
-    required: false
+    required: false,
   },
   recuperaciones: {
     type: Number,
-    required: false
+    required: false,
   },
   valoracion: {
     type: Number,
-    required: false
+    required: false,
   },
   tiros: {
-    type: tirosSchema,  // Incluye el esquema de `tiros`
-    required: false
-  }
+    type: tirosSchema, // Incluye el esquema de `tiros`
+    required: false,
+  },
 });
-
 
 const partidoSchema = new Mongoose.Schema({
   id: {
     type: Number,
-    required: false
+    required: false,
   },
   fecha: {
     type: Date,
-    required: false
+    required: false,
   },
   adversario: {
     type: String,
-    required: false
+    required: false,
   },
   puntosPropioClub: {
     type: Number,
-    required: false
+    required: false,
   },
   puntosAdversario: {
     type: Number,
-    required: false
+    required: false,
   },
   estadisticas: {
-    type: estadisticasSchema,  // Suponiendo que "partidos" es una lista de objetos
-    required: false
-  }
+    type: estadisticasSchema, // Suponiendo que "partidos" es una lista de objetos
+    required: false,
+  },
 });
 
 const jugadorSchema = new Mongoose.Schema({
   id: {
     type: Number,
-    required: true
+    required: true,
   },
   nombre: {
     type: String,
-    required: true
+    required: true,
   },
   apellido: {
     type: String,
-    required: true
+    required: true,
   },
   nacimiento: {
     type: Date,
-    required: true
+    required: true,
   },
   club: {
     type: String,
-    required: true
+    required: true,
   },
   dorsal: {
     type: Number,
-    required: true
+    required: true,
   },
   altura: {
     type: Number,
-    required: true
+    required: true,
   },
   peso: {
     type: Number,
-    required: true
+    required: true,
   },
   partidos: {
-    type: [partidoSchema],  // Suponiendo que "partidos" es una lista de objetos
+    type: [partidoSchema], // Suponiendo que "partidos" es una lista de objetos
     default: [],
-    required: false
-  }
+    required: false,
+  },
 });
-process.env.CADUCIDAD_TOKEN = '48h';
-process.env.SEED_AUTENTICACION = process.env.SEED_AUTENTICACION ||  'este-es-el-seed-desarrollo';
+process.env.CADUCIDAD_TOKEN = "48h";
+process.env.SEED_AUTENTICACION =
+  process.env.SEED_AUTENTICACION || "este-es-el-seed-desarrollo";
 
 const usuarioSchema: Schema = new Mongoose.Schema({
-    id: { type: Number, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    created: { type: Date, required: true },
-    jugador: {  type: jugadorSchema, required: true },
+  id: { type: Number, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  created: { type: Date, required: true },
+  jugador: { type: jugadorSchema, required: true },
 });
 
-usuarioSchema.methods.toJSON = function() {   let user = this;
-    let userObject = user.toObject();
-    delete userObject.password;   return userObject;
+usuarioSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
+  return userObject;
 };
 
+const federacionSchema: Schema = new Mongoose.Schema({
+  id: { type: Number, required: true },
+  nombre: { type: String, required: true },
+  clubes: { type: [String], required: true },
+});
 /*usuarioSchema.plugin(uniqueValidator, {
     message: '{PATH} debe de ser único'
 })*/
@@ -201,7 +209,10 @@ const db: Connection = Mongoose.createConnection(
 );
 
 export const usuarioModel = db.model<IUsuario>("usuarios", usuarioSchema);
-
+export const federacionModel = db.model<IFederacion>(
+  "federaciones",
+  federacionSchema
+);
 
 //HACER QUE HAYA UNA FUNCION QUE ABRA LA COLECCION COMIDAAS Y OTRA PERSONAS
 
@@ -211,14 +222,14 @@ function openDb(): Promise<any> {
       .find({})
       .then((data: any) => {
         resolve(data);
-        console.log("Funciono");})
+        console.log("Funciono");
+      })
       .catch((error: Error) => {
         console.error("Error al obtener personas:", error);
         reject(error);
       });
   });
 }
-
 
 function saveDb(db: IUsuario[]): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -241,10 +252,7 @@ function saveDb(db: IUsuario[]): Promise<void> {
   });
 }
 
-
-
 export default {
   openDb,
-  saveDb
-
+  saveDb,
 } as const;
